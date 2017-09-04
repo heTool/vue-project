6305f12cd5421391fd0366225361f5f02b85d111
hetao <hetao138@gmail.com>
2017-09-04 17:15:04 +0800

    style

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,25 @@ import Router from 'vue-router'
 import Home from 'components/home/home'
 import List from 'components/list/list'
 import Recommend from 'components/recommend/recommend'
+
 Vue.use(Router)
 export default new Router({
   routes: [
-  {
-  	path:'/',
-  	redirect:'./recommend'
-  },
+    {
+      path: '/',
+      redirect: './recommend'
+    },
     {
       path: '/home',
       component: Home
     },
     {
-    	path: '/list',
-    	component: List
+      path: '/list',
+      component: List
     },
     {
-    	path: '/recommend',
-    	component: Recommend
+      path: '/recommend',
+      component: Recommend
     }
   ]
 })
